3514280755cb5e572f9a79d8a24ada24f3ba288a
aidan gollan <aidangollan@icloud.com>
2024-12-31 22:02:47 -0800

    tracking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { track } from "~/server/actions/track";
 import HomePageClient from "./clientPage";
 
-export const dynamic = 'force-dynamic';
-
 export default async function HomePage() {
   const numFailedUsers = await track({name: "views"});
   const numFailedMessages = await track({name: "message"});
