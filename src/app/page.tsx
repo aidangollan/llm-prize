41cb6c5b00ebc129d2cb870afbd734e6d5d1dafb
aidan gollan <aidangollan@icloud.com>
2024-12-31 22:07:21 -0800

    add dynamic back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { track } from "~/server/actions/track";
 import HomePageClient from "./clientPage";
 
+export const dynamic = 'force-dynamic';
+
 export default async function HomePage() {
   const numFailedUsers = await track({name: "views"});
   const numFailedMessages = await track({name: "message"});
